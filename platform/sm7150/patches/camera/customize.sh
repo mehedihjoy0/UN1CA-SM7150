427a16040035379e849ab12881646b9f83c227c1
mehedihjoy0 <mh506370@gmail.com>
2026-01-04 13:27:23 +0600

    various fixes applied

--- platform/sm7150/patches/camera/customize.sh
+++ platform/sm7150/patches/camera/customize.sh
@@ -2,13 +2,13 @@ LOG_STEP_IN "- Adding ImageTagger lib from r9qxxx"
 ADD_TO_WORK_DIR "r9qxxx" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 LOG_STEP_OUT
 
-LOG_STEP_IN "- Adding Polarr libs from r9qxxx"
-ADD_TO_WORK_DIR "r9qxxx" "system" "system/etc/public.libraries-polarr.txt" 0 0 644 "u:object_r:system_file:s0"
-ADD_TO_WORK_DIR "r9qxxx" "system" "system/lib64/libBestComposition.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "r9qxxx" "system" "system/lib64/libFeature.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "r9qxxx" "system" "system/lib64/libPolarrSnap.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "r9qxxx" "system" "system/lib64/libTracking.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "r9qxxx" "system" "system/lib64/libYuv.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+LOG_STEP_IN "- Adding Polarr libs from a73xqxx"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/etc/public.libraries-polarr.txt" 0 0 644 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libBestComposition.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libFeature.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libPolarrSnap.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libTracking.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libYuv.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
 LOG_STEP_OUT
 
 LOG_STEP_IN "- Adding camera libs from r9qxxx"
@@ -25,6 +25,7 @@ LOG_STEP_OUT
 LOG_STEP_IN "- Replacing MIDAS config files with r9qxxx"
 DELETE_FROM_WORK_DIR "vendor" "etc/midas"
 ADD_TO_WORK_DIR "r9qxxx" "vendor" "etc/midas" 0 2000 755 "u:object_r:vendor_configs_file:s0"
+sed -i "s/a73xq/${TARGET_CODENAME}/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
 LOG_STEP_OUT
 
 LOG_STEP_IN "- Replacing singletake config files with r9qxxx"
