9877069afe36a2f7413a5e59fb2eea6b50250df6
mehedihjoy0 <mh506370@gmail.com>
2026-01-25 07:27:14 +0600

    Add 'sm7150' platform

---
+++ platform/sm7150/patches/miscs/customize.sh
@@ -0,0 +1,27 @@
+LOG_STEP_IN "- Adding \"ro.netflix.bsp_rev\" prop with \"Q7250-19133-1\" in /system/system/build.prop"
+EVAL "sed -i \"/ro.smps.gain.spk/i ro.netflix.bsp_rev=Q7250-19133-1\" \"$WORK_DIR/system/system/build.prop\""
+LOG_STEP_OUT 
+
+LOG_STEP_IN "- Removing frp"
+SET_PROP "product" "ro.frp.pst" --delete
+SET_PROP "vendor" "ro.frp.pst" --delete
+LOG_STEP_OUT 
+
+LOG_STEP_IN "- Removing media.extractor.sec.pcm-32bit"
+SET_PROP "system" "media.extractor.sec.pcm-32bit" --delete
+LOG_STEP_OUT 
+
+LOG_STEP_IN "- Fixing edge lighting"
+SET_PROP "system" "ro.factory.model" "SM-M515F"
+LOG_STEP_OUT 
+
+LOG_STEP_IN "- Increasing audio buffer size to 1024"
+SET_PROP "vendor" "vendor.audio.offload.buffer.size.kb" "1024"
+LOG_STEP_OUT 
+
+LOG_STEP_IN "- Fixing Random Reboot..."
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "bin/hw/vendor.samsung.hardware.tlc.iccc@1.0-service"
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "etc/init/vendor.samsung.hardware.tlc.iccc@1.0-service.rc"
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "lib64/vendor.samsung.hardware.tlc.iccc@1.0.so"
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "lib64/vendor.samsung.hardware.tlc.iccc@1.0-impl.so"
+LOG_STEP_OUT
