a9b6f5e4bb1c3691a3eef92ea684980c1e0a7df4
Mehedi H Joy <mh506370@gmail.com>
2026-09-15 09:10:07 +0000

    scripts/download_fw.sh: use samloader-rs instead
    Signed-off-by: Mehedi H Joy
    <mh506370@gmail.com>

--- scripts/extract_fw.sh
+++ scripts/extract_fw.sh
@@ -392,8 +392,8 @@ for i in "${FIRMWARES[@]}"; do
 
     DOWNLOADED_FIRMWARE="$(cat "$ODIN_DIR/${MODEL}_${CSC}/.downloaded")"
 
-    BL_TAR="$(find "$ODIN_DIR/${MODEL}_${CSC}" -name "BL_$(cut -d "/" -f 1 -s <<< "$DOWNLOADED_FIRMWARE")*.md5" | sort -r | head -n 1)"
-    AP_TAR="$(find "$ODIN_DIR/${MODEL}_${CSC}" -name "AP_$(cut -d "/" -f 1 -s <<< "$DOWNLOADED_FIRMWARE")*.md5" | sort -r | head -n 1)"
+    BL_TAR="$(find "$ODIN_DIR/${MODEL}_${CSC}" -name "BL_*.md5" | sort -r | head -n 1)"
+    AP_TAR="$(find "$ODIN_DIR/${MODEL}_${CSC}" -name "AP_*.md5" | sort -r | head -n 1)"
 
     if [ ! "$BL_TAR" ]; then
         LOG "\033[0;31m! No BL tar found\033[0m"
